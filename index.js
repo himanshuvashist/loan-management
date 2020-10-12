7f527e64f6470f471b97ab798e9b6c14b9e07810
himanshuvashist <35025815+himanshuvashist@users.noreply.github.com>
2020-10-13 03:41:03 +0530

    add jwt token system

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 // const logger = require('morgan')
-const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const user = require('./models/user')
+const jwt = require('jsonwebtoken')
 app.use(express.json())
+
 const result = dotenv.config()
 
 if (result.error) {
@@ -43,11 +44,74 @@ app.post('/login', (req, res) => {
             if (docs == null) return res.sendStatus(404) //not found
             bcrypt.compare(req.body.password, docs.password, (err, result) => {
                 if (err) return res.sendStatus(500)
-                if (result) return res.send('Logged In')
-                return res.send('Wrong Info')
+
+                var accessToken = jwt.sign(
+                    { foo: 'bar' },
+                    process.env.JWT_ACCESS_TOKEN_SECRET_KEY,
+                    { expiresIn: '20m' }
+                )
+                var refreshToken = jwt.sign(
+                    { foo: 'bar' },
+                    process.env.JWT_REFRESH_TOKEN_SECRET_KEY
+                )
+
+                console.log(accessToken)
+                console.log(refreshToken)
+                if (result)
+                    return res.json({ accessToken, refreshToken, loggedin: 1 })
+                return res.json({ loggedin: 0 })
             })
         })
     }
+})
+
+app.post('/token', (req, res) => {
+    const { token } = req.body
+
+    if (!token) {
+        return res.sendStatus(401)
+    }
+    // if DB does not contains the RequestToken in it then
+    // return res.sendStatus(403)
+
+    jwt.verify(token, process.env.JWT_REFRESH_TOKEN_SECRET_KEY, (err, data) => {
+        if (err) return res.sendStatus(403)
+
+        const accessToken = jwt.sign(
+            { data: 'data' },
+            process.env.JWT_ACCESS_TOKEN_SECRET_KEY,
+            { expiresIn: '20m' }
+        )
+
+        res.json({ accessToken })
+    })
+})
+//authentication middleware
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1]
+
+        jwt.verify(
+            token,
+            process.env.JWT_ACCESS_TOKEN_SECRET_KEY,
+            (err, data) => {
+                if (err) console.log(`Error ${err}`)
+                if (err) return res.sendStatus(403) //forbidden
+                console.log(data)
+                data.user = user
+                console.log(data)
+                next()
+            }
+        )
+    } else {
+        res.sendStatus(401) // unauthorized
+    }
+}
+
+app.post('/getdata', authenticateJWT, (req, res) => {
+    res.json([{ book1: 'harrypotter', book2: 'dungeon' }])
 })
 
 async function alreadyExists(data) {
@@ -56,7 +120,7 @@ async function alreadyExists(data) {
 
     let u = await user.countDocuments({ userName: data.name })
     if (u != 0) return true
-    
+
     return false
 }
 
@@ -79,6 +143,14 @@ app.post('/register', (req, res) => {
         })
         res.send({ response: 'successfully inserted' })
     })
+})
+
+app.post('/logout', (req, res) => {
+    const { token } = req.body
+
+    // remove the refrest token from the DB
+
+    res.sendStatus(200)
 })
 
 app.listen(port, () => {
