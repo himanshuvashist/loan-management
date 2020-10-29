555ec2fe0688092f3b3acfa5ac9883152abb7e31
himanshuvashist <35025815+himanshuvashist@users.noreply.github.com>
2020-10-29 12:22:00 +0530

    add origin to the aws address

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsConfig = {
-  origin: 'http://localhost:3001',
+  origin: 'http://ec2-15-207-236-232.ap-south-1.compute.amazonaws.com',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
